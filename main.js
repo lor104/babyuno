805f6a4aebd7fdb7493c7c7a8d50db3f85f9268a
lor104 <lauren.rosentzveig@gmail.com>
2017-10-06 18:11:13 -0400

    add jquery UI

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
 
+  $('#bubble1').draggable({
+    containment: "window",
+    zIndex : "1",
+    revert : "invalid"
+  });
+  $('#bubble2').draggable({
+    containment: "window",
+    zIndex : "1",
+    revert : "invalid"
+  });
 
-)}
+  $('#container').droppable({
+    accept: '.bubble',
+    drop: function(event,ui) {
+      ui.draggable.fadeOut(function() {
+        ui.draggable.remove();
+      })
+    }
+  })
+
+})
