b8592b09fc50a70fcebbc9150fbaac1a63353571
lor104 <lauren.rosentzveig@gmail.com>
2017-10-07 16:58:50 -0400

    background of container changes based on the item dragged on top

--- main.js
+++ main.js
@@ -19,9 +19,22 @@ $(document).ready(function() {
   })
 
 function fillContainer( $item ) {
-    var colour = $item.css("background-color");
+    // var image = $item.css("background");
+    var image;
+    var imageTag = $item.context.id
+    console.log($item.context.id)
+
+    switch (imageTag) {
+      case "bubble2":
+        image = 'url("moose.png") no-repeat 25%'
+        break;
+      case "bubble1":
+        image = 'red';
+        break;
+    }
+    console.log(image)
     var body = $('body');
-    $('#container').css("background-color", colour);
+    $('#container').css("background", image);
 }
 
 })
