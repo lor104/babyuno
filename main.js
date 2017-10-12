a85c50f6bfd5c68e27be6370d2184d10ec655bfb
lor104 <lauren.rosentzveig@gmail.com>
2017-10-11 22:27:01 -0400

    adjust tested functions with actual website images, moose appears on onesie when dragged

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $('#bubble1').draggable({
+  $('#moose').draggable({
     containment: "window",
     zIndex : "1",
     revert : true
@@ -25,17 +25,17 @@ function fillContainer( $item ) {
     console.log($item.context.id)
 
     switch (imageTag) {
-      case "bubble2":
-        image = 'url("moose.png") no-repeat'
+      case "moose":
+        image = 'images/moose-red.svg'
         break;
-      case "bubble1":
+      case "bubble2":
         image = 'red';
         break;
     }
     console.log(image)
     var body = $('body');
-    $('#container').css("background", image);
-    $('#container').css("background-size", "50%");
+    $('#onesie-design').attr("src", image);
+    // $('#container').css("background-size", "50%");
 }
 
 })
