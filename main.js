839433924f23f8a521c131418a5260ebd3495304
lor104 <lauren.rosentzveig@gmail.com>
2017-10-13 15:00:03 -0400

    add color bubbles and script to change color of onesie design when dragged over existing image

--- main.js
+++ main.js
@@ -30,40 +30,167 @@ $(document).ready(function() {
     zIndex : "1",
     revert : true
   });
+  $('#color-red').draggable({
+    containment: "window",
+    zIndex : "1",
+    revert : true
+  });
+  $('#color-yellow').draggable({
+    containment: "window",
+    zIndex : "1",
+    revert : true
+  });
+  $('#color-purple').draggable({
+    containment: "window",
+    zIndex : "1",
+    revert : true
+  });
+  $('#color-green').draggable({
+    containment: "window",
+    zIndex : "1",
+    revert : true
+  });
 
   $('#container').droppable({
-    accept: '.icon',
+    accept: '.drop',
     drop: function(event,ui) {
       fillContainer( ui.draggable );
     }
   })
 
+var design;
 function fillContainer( $item ) {
     var image;
-    var imageTag = $item.context.id
 
-    switch (imageTag) {
-      case "icon-moose":
-        image = 'images/moose-red.svg'
-        break;
-      case "icon-owl":
-        image = 'images/owl-purple.svg'
-        break;
-      case "icon-pig":
-        image = 'images/pig-red.svg'
-        break;
-      case "icon-trex":
-        image = 'images/trex-green.svg'
-        break;
-      case "icon-monkey":
-        image = 'images/monkey-purple.svg'
-        break;
-      case "icon-sheep":
-        image = 'images/sheep-yellow.svg'
-        break;
+    var imageTag = $item.context.id;
+    var element = document.getElementById(imageTag);
+    // console.log(element)
+    console.log("Is this a color: " + element.classList.contains("color"));
+    console.log("Is this a icon: " + element.classList.contains("icon"));
+
+    if (element.classList.contains("icon")) {
+      switch (imageTag) {
+        case "icon-moose":
+          image = 'images/moose-red.svg';
+          design = 'moose';
+          break;
+        case "icon-owl":
+          image = 'images/owl-purple.svg';
+          design = 'owl';
+          break;
+        case "icon-pig":
+          image = 'images/pig-red.svg';
+          design = 'pig';
+          break;
+        case "icon-trex":
+          image = 'images/trex-green.svg';
+          design = 'trex';
+          break;
+        case "icon-monkey":
+          image = 'images/monkey-purple.svg';
+          design = 'monkey';
+          break;
+        case "icon-sheep":
+          image = 'images/sheep-yellow.svg';
+          design = 'sheep';
+          break;
+      }
+    var body = $('body');
+    console.log(design)
+    $('#onesie-design').attr("src", image);
+  }
+  if (element.classList.contains("color")) {
+    console.log("this is a color");
+    console.log(imageTag)
+    if (imageTag === "color-red") {
+      switch (design) {
+        case "moose":
+          image = "images/moose-red.svg";
+          break;
+        case "owl":
+          image = "images/owl-red.svg";
+          break;
+        case "pig":
+          image = "images/pig-red.svg";
+          break;
+        case "trex":
+          image = "images/trex-red.svg";
+          break;
+        case "monkey":
+          image = "images/monkey-red.svg";
+          break;
+        case "sheep":
+          image = "images/sheep-red.svg";
+          break;
+      }
+    } else if (imageTag === "color-yellow") {
+      switch (design) {
+        case "moose":
+          image = "images/moose-yellow.svg";
+          break;
+        case "owl":
+          image = "images/owl-yellow.svg";
+          break;
+        case "pig":
+          image = "images/pig-yellow.svg";
+          break;
+        case "trex":
+          image = "images/trex-yellow.svg";
+          break;
+        case "monkey":
+          image = "images/monkey-yellow.svg";
+          break;
+        case "sheep":
+          image = "images/sheep-yellow.svg";
+          break;
+      }
+    } else if (imageTag === "color-green") {
+      switch (design) {
+        case "moose":
+          image = "images/moose-green.svg";
+          break;
+        case "owl":
+          image = "images/owl-green.svg";
+          break;
+        case "pig":
+          image = "images/pig-green.svg";
+          break;
+        case "trex":
+          image = "images/trex-green.svg";
+          break;
+        case "monkey":
+          image = "images/monkey-green.svg";
+          break;
+        case "sheep":
+          image = "images/sheep-green.svg";
+          break;
+      }
+    } else if (imageTag === "color-purple") {
+      switch (design) {
+        case "moose":
+          image = "images/moose-purple.svg";
+          break;
+        case "owl":
+          image = "images/owl-purple.svg";
+          break;
+        case "pig":
+          image = "images/pig-purple.svg";
+          break;
+        case "trex":
+          image = "images/trex-purple.svg";
+          break;
+        case "monkey":
+          image = "images/monkey-purple.svg";
+          break;
+        case "sheep":
+          image = "images/sheep-purple.svg";
+          break;
+      }
     }
     var body = $('body');
+    console.log(design)
     $('#onesie-design').attr("src", image);
+  }
 }
 
 $('#icon-moose').mouseover(function() {$('#border-moose').toggleClass("active-rotate")});
